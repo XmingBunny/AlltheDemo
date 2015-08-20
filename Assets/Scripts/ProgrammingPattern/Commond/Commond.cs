c6ba5536849d6c73b898798bbb0c5c0114420e1e
SiuMing <2457944792@qq.com>
2015-08-20 17:04:50 +0800

    新建了游戏设计模式文件夹

---
+++ Assets/Scripts/ProgrammingPattern/Commond/Commond.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace AllTheDemo.ProgrammingPattern.Commond
+{
+    public class Commond
+    {
+        public virtual void excute(Animator ani)
+        {
+ 
+        }
+    }
+
+    public class JumpCommnod : Commond
+    {
+        public override void excute(Animator ani)
+        {
+
+        }
+    }
+
+    public class FireGunCommnod : Commond
+    {
+        public virtual void excute(Animator ani)
+        {
+
+        }
+    }
+
+    public class SneakCommnod : Commond
+    {
+        public override void excute(Animator ani)
+        {
+
+        }
+    }
+
+    public class ShoutCommnod : Commond
+    {
+        public override void excute(Animator ani)
+        {
+
+        }
+    }
+}
