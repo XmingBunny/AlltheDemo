0e880e1afc23de4c0262d6c257a49687205632ac
SiuMing <2457944792@qq.com>
2015-08-21 17:06:26 +0800

    增加了残影脚本,可是写出来的效果好像分裂一样..

---
+++ Assets/Scripts/MoverMent/Ghost.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ghost : MonoBehaviour
+{
+    static int count;
+
+    public float Frequency;
+    public int MaxCount;
+
+    float timer = 0f;
+
+    void Update()
+    {
+        if (timer > Frequency && count <= MaxCount)
+        {
+            GameObject.Instantiate(transform, transform.position, transform.rotation);
+            count++;
+            timer = 0f;
+        }
+
+        timer += Time.deltaTime;
+    }
+}
